654da995723e8aa5294318c23feb81e5c42a90be
aziz <azizhamza005@gmail.com>
2022-10-19 15:24:29 +0100

    page services

--- src/components/Bordure.js
+++ src/components/Bordure.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import "./Bordure.css";
 
-
+   
 export default function Bordure() {
+  var title=window.location.href;
+  var titre=title.split("/");
   return (
     <div class="breadcrumbs d-flex align-items-center">
             <div class="container position-relative d-flex flex-column align-items-center">
-                <h2 >contact</h2>
+                <h2 >{titre[3]}</h2>
                 <ol>
                 <li><a href="index.html">Home</a></li>
-                <li>contact</li>
+                <li>{titre[3]}</li>
                     </ol>
 
             </div>
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,11 +1,156 @@
 import React from "react";
+import Bordure from "../components/Bordure";
+import "./Services.css";
+import'bootstrap/dist/css/bootstrap.min.css';
+import image1 from "../assets/blog-1.jpg";
+
+
 
 function Services() {
 
-
     return (
-        <div>
-            
+        <div className="servicess">
+            <Bordure />
+            <section id="alt-services-2" className="alt-services section-bg">
+                <div className="container" data-aos="fade-up">
+
+                <div className="row justify-content-around gy-4">
+                    <div className="col-lg-5 d-flex flex-column justify-content-center">
+                    <h3>Non quasi officia eum nobis et rerum epudiandae rem voluptatem</h3>
+                    <p>Maxime quia dolorum alias perspiciatis. Earum voluptatem sint at non. Ducimus maxime minima iste magni sit praesentium assumenda minus. Amet rerum saepe tempora vero.</p>
+
+                    <div className="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="100">
+                        <i className="bi bi-easel flex-shrink-0"></i>
+                        <div>
+                        <h4><a href="" className="stretched-link">Lorem Ipsum</a></h4>
+                        <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+                        </div>
+                    </div>
+
+                    <div className="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="200">
+                        <i className="bi bi-patch-check flex-shrink-0"></i>
+                        <div>
+                        <h4><a href="" className="stretched-link">Nemo Enim</a></h4>
+                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+                        </div>
+                    </div>
+
+                    <div className="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="300">
+                        <i className="bi bi-brightness-high flex-shrink-0"></i>
+                        <div>
+                        <h4><a href="" className="stretched-link">Dine Pad</a></h4>
+                        <p>Explicabo est voluptatum asperiores consequatur magnam. Et veritatis odit. Sunt aut deserunt minus aut eligendi omnis</p>
+                        </div>
+                    </div>
+
+                    <div className="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="400">
+                        <i className="bi bi-brightness-high flex-shrink-0"></i>
+                        <div>
+                        <h4><a href="" className="stretched-link">Tride clov</a></h4>
+                        <p>Est voluptatem labore deleniti quis a delectus et. Saepe dolorem libero sit non aspernatur odit amet. Et eligendi</p>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className="col-lg-6 img-bg"  data-aos="zoom-in" data-aos-delay="100"></div>
+                </div>
+
+                </div>
+            </section>
+
+            <section id="alt-services" className="alt-services">
+                <div className="container" data-aos="fade-up">
+
+                <div className="row justify-content-around gy-4">
+                    <div className="col-lg-6 img-bg" data-aos="zoom-in" data-aos-delay="100"></div>
+
+                    <div className="col-lg-5 d-flex flex-column justify-content-center">
+                    <h3>Enim quis est voluptatibus aliquid consequatur fugiat</h3>
+                    <p>Esse voluptas cumque vel exercitationem. Reiciendis est hic accusamus. Non ipsam et sed minima temporibus laudantium. Soluta voluptate sed facere corporis dolores excepturi</p>
+
+                    <div className="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="100">
+                        <i className="bi bi-easel flex-shrink-0"></i>
+                        <div>
+                        <h4><a href="" className="stretched-link">Lorem Ipsum</a></h4>
+                        <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+                        </div>
+                    </div>
+
+                    <div className="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="200">
+                        <i className="bi bi-patch-check flex-shrink-0"></i>
+                        <div>
+                        <h4><a href="" className="stretched-link">Nemo Enim</a></h4>
+                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+                        </div>
+                    </div>
+
+                    <div className="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="300">
+                        <i className="bi bi-brightness-high flex-shrink-0"></i>
+                        <div>
+                        <h4><a href="" className="stretched-link">Dine Pad</a></h4>
+                        <p>Explicabo est voluptatum asperiores consequatur magnam. Et veritatis odit. Sunt aut deserunt minus aut eligendi omnis</p>
+                        </div>
+                    </div>
+
+                    <div className="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="400">
+                        <i className="bi bi-brightness-high flex-shrink-0"></i>
+                        <div>
+                        <h4><a href="" className="stretched-link">Tride clov</a></h4>
+                        <p>Est voluptatem labore deleniti quis a delectus et. Saepe dolorem libero sit non aspernatur odit amet. Et eligendi</p>
+                        </div>
+                    </div>
+
+                    </div>
+                </div>
+
+                </div>
+            </section>
+            <section id="alt-services-2" className="alt-services section-bg">
+                <div className="container" data-aos="fade-up">
+
+                <div className="row justify-content-around gy-4">
+                    <div className="col-lg-5 d-flex flex-column justify-content-center">
+                    <h3>Non quasi officia eum nobis et rerum epudiandae rem voluptatem</h3>
+                    <p>Maxime quia dolorum alias perspiciatis. Earum voluptatem sint at non. Ducimus maxime minima iste magni sit praesentium assumenda minus. Amet rerum saepe tempora vero.</p>
+
+                    <div className="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="100">
+                        <i className="bi bi-easel flex-shrink-0"></i>
+                        <div>
+                        <h4><a href="" className="stretched-link">Lorem Ipsum</a></h4>
+                        <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+                        </div>
+                    </div>
+
+                    <div className="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="200">
+                        <i className="bi bi-patch-check flex-shrink-0"></i>
+                        <div>
+                        <h4><a href="" className="stretched-link">Nemo Enim</a></h4>
+                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+                        </div>
+                    </div>
+
+                    <div className="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="300">
+                        <i className="bi bi-brightness-high flex-shrink-0"></i>
+                        <div>
+                        <h4><a href="" className="stretched-link">Dine Pad</a></h4>
+                        <p>Explicabo est voluptatum asperiores consequatur magnam. Et veritatis odit. Sunt aut deserunt minus aut eligendi omnis</p>
+                        </div>
+                    </div>
+
+                    <div className="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="400">
+                        <i className="bi bi-brightness-high flex-shrink-0"></i>
+                        <div>
+                        <h4><a href="" className="stretched-link">Tride clov</a></h4>
+                        <p>Est voluptatem labore deleniti quis a delectus et. Saepe dolorem libero sit non aspernatur odit amet. Et eligendi</p>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className="col-lg-6 img-bg"  data-aos="zoom-in" data-aos-delay="100"></div>
+                </div>
+
+                </div>
+            </section>
         </div>
     );
 }
